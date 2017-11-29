2a2d2b443b1228bb01d2364b3f73f75aebfe7534
Erik Nygren <erik.nygren6@gmail.com>
2017-11-29 18:46:50 +0000

    Heyfocus pommodoro + Khaled support

---
+++ pomodoro_counter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ~/.dotfiles/quote_picker.sh
+
+pomodoros=$(grep -o "^$(date '+%a %b %d %Y')" ~/focus-sessions.txt | wc -l | sed -e 's/^[ \t]*//')
+echo "You've done $pomodoros pomodoros today. Remember: $selectedexpression"
