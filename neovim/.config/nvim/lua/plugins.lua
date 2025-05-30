4d68f7ea05a882c8e4dfeca8c2d34db9a1a7bcba
Erik Nygren <erik.nygren6@gmail.com>
2025-05-30 10:12:47 +0100

    remove cmp-buffer, don't like

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -48,7 +48,6 @@ return require('lazy').setup({
       -- Autocompletion
       { 'hrsh7th/nvim-cmp' },
       { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-buffer' },
       { 'hrsh7th/cmp-path' },
       { 'hrsh7th/cmp-cmdline' },
       { 'L3MON4D3/LuaSnip' },
